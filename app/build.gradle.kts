77dfe64386e5bac5f44a72461fbe3d687b1b3e6a
Hong Le Huu Khang <hongkhang1250@Gmail.com>
2025-04-03 18:10:50 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,10 +6,12 @@ plugins {
 android {
     namespace = "com.example.weatherapp"
     compileSdk = 35
-
+    buildFeatures{
+        viewBinding = true
+    }
     defaultConfig {
         applicationId = "com.example.weatherapp"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -42,7 +44,19 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.car.ui.lib)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.airbnb.android:lottie:6.1.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.google.android.material:material:1.11.0") // Cho SearchView hoạt động tốt
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2") // Cho MVVM
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2") // LiveData
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // Coroutines
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0") // Log API requests
+
+
+
 }
